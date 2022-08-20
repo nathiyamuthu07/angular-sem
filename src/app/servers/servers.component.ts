29c551e90d2f396cbf2a48c2f7e01e9532a4254f
Muthu Elumalai Kamalakannan <Kamalakannan_Muthu_Elumalai@cat.com>
2022-08-20 20:32:43 +0530

    Nathiya new angular project in git

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  // allowNewServer = false;
+  // serverCreationStatus = 'no server was created!';
+  // serverName = 'TestServer';
+  // serverCreated = false;
+  // servers=['TestServer' , 'TestServer 2']
+
+  constructor() { 
+    // setTimeout(() => {
+    //   this.allowNewServer = true;
+    //  } ,2000);
+  }
+
+  ngOnInit() {
+  }
+    onCreateServer() {
+      // this.serverCreated = true;
+      // this.servers.push(this.serverName)
+      // this.serverCreationStatus = 'Server was Created! Name is '+ this.serverName;
+    }
+    // onUpdateServerName(event: Event){
+    //   this.serverName = (<HTMLInputElement>event.target).value
+    // }
+  
+
+}
